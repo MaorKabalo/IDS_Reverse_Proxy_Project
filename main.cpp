50b7099cfaf89e24b6989cd627950c57b2f37302
Ripi01 <itairipvs@gmail.com>
2023-12-14 10:58:48 -0500

    add server.py and client.py

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    boost::asio::io_context io_context;
+    short int server_port, proxy_port;
+    std::string ip;
+    std::cout<<"enter server ip";
+    std::cin>>ip;
+    std::cout<<"enter server port";
+    std::cin>>server_port;
+    std::cout<<"enter proxy port";
+    std::cin>>proxy_port;
+    ReverseProxy reverse_proxy(io_context, proxy_port, ip, server_port);
     return 0;
 }
---
+++ server.py
@@ -0,0 +1,27 @@
+import socket
+
+LISTEN_PORT = 8888
+REVERSE_PROXY_IP = "localhost"
+REVERSE_PROXY_PORT = 9090
+
+def main():
+    with socket.socket() as listening_sock:
+        listening_sock.bind(('', LISTEN_PORT))
+        listening_sock.listen(1)
+        openPorts()
+        client_soc, client_address = listening_sock.accept()
+        with socket.socket(client_soc, client_address) as client_soc:
+            server_msg = socket.recv(1024)
+            server_msg = server_msg.decode()
+            print(server_msg)
+            client_soc.sendall(server_msg)
+
+def openPorts():
+    with socket.socket():
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = (REVERSE_PROXY_IP, REVERSE_PROXY_PORT)
+        sock.connect(server_address)
+        sock.sendall(LISTEN_PORT+"")
+
+if __name__=="__main__":
+    main()
